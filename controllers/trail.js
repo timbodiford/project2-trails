02d379ad07ac0c6ef84948519f9bafcb01059bba
Tim Bodiford <tcbodiford@gmail.com>
2019-07-01 09:25:35 -0400

    forgot to do a commit from the weekend

--- controllers/trail.js
+++ controllers/trail.js
@@ -11,9 +11,44 @@ const trailRouter = express.Router()
 trailRouter.get('/', (req, res) => {
   trailApi.getTrails()
   .then((trails) => {
-    res.render('trails/trails')
+    res.render('trails/trails', { trails })
   })
 })
+
+trailRouter.post('/', (req, res) => {
+  trailApi.addTrail(req.body)
+  .then(() => {
+    res.redirect('/trails')
+  })
+})
+
+trailRouter.get('/new', (req, res) => {
+  res.render('trails/newTrailForm')
+})
+
+trailRouter.get('/:trailId', (req, res) => {
+  trailApi.getTrail(req.params.trailId)
+  .then((trail) => {
+    res.render('trail/trail', {trail} )
+  })
+})
+
+trailRouter.delete('/:trailId', (req, res) => {
+  trailApi.deleteTrail(req.params.trailId)
+  .then((trail) => {
+    res.redirect('/trails')
+  })
+})
+
+trailRouter.put('/:trailId', (req, res) => {
+  trailApi.editTrail(req.params.trailId, req.body)
+  .then(() => {
+    res.redirect('/trails')
+  })
+})
+
+
+
 
 
 module.exports = {
--- models/trail.js
+++ models/trail.js
@@ -19,7 +19,7 @@ const TrailSchema = new mongoose.Schema({
   rating: {
     type: Number,
   },
-  descrption: {
+  description: {
     type: String,
   },
   imgLink: {
@@ -36,7 +36,30 @@ function getTrails() {
   return TrailCollection.find()
 }
 
+function addTrail(trailObject) {
+  return TrailCollection.create(trailObject)
+}
+
+function getTrail(trailId) {
+  return TrailCollection.findById(trailId)
+}
+
+function deleteTrail(trailId) {
+  return TrailCollection.findByIdAndDelete(trailId)
+}
+
+function editTrail(trailId, trailObject) {
+  return TrailCollection.findByIdAndUpdate(trailId, trailObject)
+}
+
+
+
+
 
 module.exports = {
-  getTrails
+  getTrails,
+  getTrail,
+  addTrail,
+  deleteTrail,
+  editTrail
 }
